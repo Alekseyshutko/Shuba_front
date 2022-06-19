d1389e0797cca0c22669000bb93938dd484193dd
bazz-00 <baziukkostia@gmail.com>
2022-06-19 11:43:46 +0300

    added homepage template

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import requests
+from flask import render_template
 
 app = Flask(__name__)
-
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 @app.route("/")
 def index():
-    return "shuba"
+    return render_template('base.html', title='Главная страница', menu=menu)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
