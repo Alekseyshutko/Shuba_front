672d75c6bc210b6d83125a54b4232d62214bed0d
Alekseyshutko <Aleksey_shutko@rambler.ru>
2022-08-29 21:59:15 +0300

    heroku config

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 
 class Config:
-    API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
-    #API_URL = os.getenv("API_URL", "https://shubabackend.herokuapp.com")
+    #API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
+    API_URL = os.getenv("API_URL", "https://shubabackend.herokuapp.com")
     SECRET_KEY = os.getenv("SECRET_KEY")
     BUCKET_NAME = os.getenv("BUCEKT_NAME", "shuba")
     S3_LOCATION = os.getenv("S3_LOCATION", "eu-central-1")
